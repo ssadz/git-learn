86c847a0640164638f777c3861edc06c2e4282b1
mfc <mfcdlut08@gmal.com>
2025-04-08 18:37:45 +0800

    添加打印欢迎信息的函数

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 
+// 打印欢迎信息的函数
+void printWelcome() {
+    std::cout << "欢迎学习Git版本控制!" << std::endl;
+}
+
 int main() {
     std::cout << "Hello, Git!" << std::endl;
+    printWelcome();
     return 0;
-} 
+}
